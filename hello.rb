c52aa0f6b2d229713920e2ee60b135b863aeb347
RakeshKumarSharma <rakesh.sharma@nature.com>
2014-05-28 15:16:29 +0100

    this is to commit stash

--- hello.rb
+++ hello.rb
@@ -1,2 +1,2 @@
 This is a sample file for playing around with GitHub.
-This is for unstaging a file.THis is for stashing the changes.
+This is for unstaging a file.THis is for stashing the changes.This is second stashing change.
